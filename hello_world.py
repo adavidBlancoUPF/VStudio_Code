12819d8d3fc230a3f5f06c9086dbcc3fd228b951
Ángel David Blanco Casares <ablancocasares@gmail.com>
2022-03-28 16:41:06 +0200

    merge of noFakeTeam with master

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,5 @@
 #%%
-<<<<<<< HEAD
-var = "Hello World This is fakeTeamWork"
-=======
 var = "Hello World This is BugFix and the work of my mates was wrong"
->>>>>>> noFakeTeam
 print(var)
 #%%
 x = 0
